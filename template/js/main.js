868daf045d3bf0d5ee3acfd2d672902db6611c11
fucho <fmgbeoma@yahoo.com>
2017-09-23 21:04:33 +0100

    FEATURE UPDATED: add new recipe; multiple ingredients and quantity

---
+++ template/js/main.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function() {
+    // Add new ingredient select and text field
+    document.getElementById('add-more-ingredient').addEventListener('click', () => {
+        let addMore = document.createElement('div');
+        addMore.innerHTML = `<div class="col-sm-4 mb-1">
+                            <select class="btn dropdown-toggle" name="quantity" id="quantity">
+                                    <option value="">Select quantity</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-8">
+                            <div>
+                                <input type="text" name="ingredient[]" id="ingredient" class="form-control" required>
+                            </div>
+                        </div>`;
+        document.getElementById('recipe-quantity').append(addMore);
+    });
+
+    document.querySelector('input[type="file"]').addEventListener('change', () =>  {
+        if (this.files && this.files[0]) {
+            let img = document.querySelector('img');
+            img.src = URL.createObjectURL(this.files[0]);
+        }
+    });
+});
