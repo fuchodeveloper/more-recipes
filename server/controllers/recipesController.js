78fbe60f08e05ed42b0c4b869aa226fd2fe6905f
fucho <fmgbeoma@yahoo.com>
2017-10-05 17:53:12 +0100

    chore: clean up code and add migration for upvotes

--- server/controllers/recipesController.js
+++ server/controllers/recipesController.js
@@ -23,7 +23,10 @@ const recipeController = {
     };
 
     const token = request.headers['x-access-token'];
-    if (!token) return response.status(401).send({ auth: false, message: 'No token provided.' });
+    if (!token) {
+      return response.status(401)
+        .send({ auth: false, message: 'No token provided.' });
+    }
 
     const decodedId = jwt.verify(token, secret);
 
@@ -33,7 +36,10 @@ const recipeController = {
     }
     User.findById(decodedId.data.id)
       .then((user) => {
-        if (!user) response.status(404).json({ errorCode: 404, message: 'User not found.' });
+        if (!user) {
+          response.status(404)
+            .json({ errorCode: 404, message: 'User not found.' });
+        }
         return Recipe.create({
           userId: decodedId.data.id,
           recipeName: request.body.recipeName,
@@ -42,10 +48,13 @@ const recipeController = {
           recipeDirection: request.body.recipeDirection,
           recipeImage: request.body.recipeImage
         })
-          .then(recipe => response.status(201).json({ message: 'Recipe created successfully ', recipe }))
-          .catch(error => response.status(404).send(error.message));
+          .then(recipe => response.status(201)
+            .json({ message: 'Recipe created successfully ', recipe }))
+          .catch(error => response.status(404)
+            .send(error.message));
       })
-      .catch(error => response.status(404).send(error.message));
+      .catch(error => response.status(404)
+        .send(error.message));
   },
 
   /**
@@ -59,26 +68,37 @@ const recipeController = {
       .findById(request.params.id)
       .then((recipe) => {
         if (!recipe) {
-          response.status(404).json({ message: 'Recipe not found' });
+          response.status(404)
+            .json({ message: 'Recipe not found' });
         }
         return recipe
           .update({ views: recipe.views + 1 });
       })
       .then((recipe) => {
         if (!recipe) {
-          response.status(404).json({ message: 'Recipe not found' });
+          response.status(404)
+            .json({ message: 'Recipe not found' });
         }
-        response.status(200).json({ recipe });
+        response.status(200)
+          .json({ recipe });
       })
-      .catch(error => response.status(400).json({ error: error.message }));
+      .catch(error => response.status(400)
+        .json({ error: error.message }));
   },
 
+  /**
+   * Return all recipes;
+   * @param {any} request
+   * @param {any} response
+   * @returns {obj} obj
+   */
   getAll(request, response) {
-    // return all recipes;
     return Recipe
       .findAll()
-      .then(recipes => response.status(200).json({ message: recipes }))
-      .catch(error => response.status(400).json(error));
+      .then(recipes => response.status(200)
+        .json({ message: recipes }))
+      .catch(error => response.status(400)
+        .json(error));
   },
 
   delete(request, response) {
@@ -86,23 +106,28 @@ const recipeController = {
       .findById(request.params.id)
       .then((recipe) => {
         if (!recipe) {
-          response.status(404).json({ message: 'Recipe not found' });
+          response.status(404)
+            .json({ message: 'Recipe not found' });
         }
         const token = request.headers['x-access-token'];
         if (!token) {
-          return response.status(401).json({ auth: false, message: 'No token provided.' });
+          return response.status(401)
+            .json({ auth: false, message: 'No token provided.' });
         } else
         if (token) {
           const decodedId = jwt.verify(token, secret);
           if (decodedId.data.id === recipe.userId) {
             return recipe
               .destroy()
-              .then(() => response.status(200).json({ message: 'Recipe deleted' }))
-              .catch(error => response.status(400).json(error));
+              .then(() => response.status(200)
+                .json({ message: 'Recipe deleted' }))
+              .catch(error => response.status(400)
+                .json(error));
           }
         }
       })
-      .catch(error => response.status(400).json(error));
+      .catch(error => response.status(400)
+        .json(error));
   }
 };
 
