69ebf4c798bf56f3e84c85302e2d7fd01751b40d
fucho <fmgbeoma@yahoo.com>
2017-11-04 18:33:13 +0100

    feature: display users favorite recipes

--- client/components/favorites/FavoriteRecipesPage.js
+++ client/components/favorites/FavoriteRecipesPage.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Header from '../../components/navigation/Header';
 import Footer from '../../components/navigation/Footer';
 import getAllFavorites from '../../action/favorites/getAllFavorites';
+import AllFavoriteRecipes from './AllFavoriteRecipes';
 
 class FavoriteRecipesPage extends React.Component {
   constructor(props) {
@@ -27,19 +28,53 @@ class FavoriteRecipesPage extends React.Component {
     setTimeout(() => this.setState({ isLoading: false }), 1000);
     this.props.getAllFavorites(userIdParam)
     .then((favorites) => {
-      console.log(favorites.data.isFound);
-      // this.setState({ details: recipe.data })
+      // console.log(favorites.data.isFound);
+      this.setState({ details: favorites.data.isFound })
     })
     .catch((error) => {
-      console.log(error);
-      // this.setState({ errors: error.response })
+      // console.log(error);
+      this.setState({ errors: error.data })
     })
   }
 
   render() {
+
+    const { isLoading } = this.state;
+    
+    if (isLoading) {
+      return (
+        <h2 className="text-center">Loading...</h2>
+      );
+    }
+    
     return (
       <div>
+        {/* Header component for navigation */}
+        <Header />
 
+        <div className="container margin-top-70">
+
+          <div>
+              <h1 className="text-center p-4 center-hero-title">All Your Favorite Recipes In One Place</h1>
+          </div>
+
+          <div className="margin-top-50 margin-bottom-50"/>
+
+          <div className="row">
+            {
+              Object
+                .keys(this.state.details)
+                .map(key => <AllFavoriteRecipes key={key} details={this.state.details[key]} />)
+            }
+            
+          </div>
+
+        </div>
+
+        <div className="clearfix m-5"/>
+
+        {/* Display footer  */}
+        <Footer />
       </div>
     )
   }
