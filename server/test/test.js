d2159bd24b612a6a5802ad3b251852ef28cb80ba
Fredrick Mgbeoma <fuchodeveloper@Mgbeomas-MacBook-Pro.local>
2017-11-15 17:19:47 +0100

    (ch): cleanup - code cleanup - add hound.yml file

--- server/test/test.js
+++ server/test/test.js
@@ -7,31 +7,6 @@ chai.use(chaiHttp);
 
 describe('Tests for more-recipes API endpoints', () => {
   describe('Handle all required endpoints', () => {
-    describe('GET api/v1/recipes', () => {
-      it('should GET all recipes', (done) => {
-        chai.request(app).get('/api/v1/recipes').end((error, response) => {
-          expect(response).to.have.status(200);
-          done();
-        });
-      });
-
-      it('should not GET any recipes', (done) => {
-        chai.request(app).get('/api/v1/recipes/100').end((error, response) => {
-          expect(response).to.have.status(404);
-          done();
-        });
-      });
-    });
-
-    describe('GET api/v1/recipes/:id', () => {
-      it('should GET a recipe', (done) => {
-        chai.request(app).get('/api/v1/recipes/1').end((error, response) => {
-          expect(response).to.have.status(200);
-          done();
-        });
-      });
-    });
-
     describe('About page', () => {
       it('should return true if "About" page does not exist', () => {
         chai.request('/api/v1/about', (error, response) => {
